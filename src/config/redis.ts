657db3b96d2a43c5cbd4e4900999ace927a59381
Benjamin-dev216 <golden.dev.216@gmail.com>
2025-05-01 04:39:38 -0700

    updated redis config

--- src/config/redis.ts
+++ src/config/redis.ts
@@ -2,7 +2,7 @@ import Redis from "ioredis";
 import "dotenv/config";
 
 export const redisClient = new Redis({
-  host: process.env.REDIS_HOST || "127.0.0.1",
+  host: process.env.REDIS_HOST,
   // port: parseInt(process.env.REDIS_PORT || "6379"),
   // password: process.env.REDIS_PASSWORD || undefined,
   db: 0,
